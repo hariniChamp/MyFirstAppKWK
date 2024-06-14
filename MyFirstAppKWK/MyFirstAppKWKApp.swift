483dfc0ebc9a95c189213046a51194a8a3c00854
Harini <harinichamp02@gmail.com>
2024-06-14 11:54:40 -0500

    Initial Commit

---
+++ MyFirstAppKWK/MyFirstAppKWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppKWKApp.swift
+//  MyFirstAppKWK
+//
+//  Created by Scholar on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppKWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
